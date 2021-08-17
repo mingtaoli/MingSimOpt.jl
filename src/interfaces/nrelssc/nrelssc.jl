e86e07792c0da5b85a405982cb4c2c8a72b1e571
Mingtao Li <mingtao@xjtu.edu.cn>
2021-08-17 09:09:02 +0800

    继续增加nrelssc的接口函数

--- src/interfaces/nrelssc/nrelssc.jl
+++ src/interfaces/nrelssc/nrelssc.jl
@@ -2,14 +2,27 @@ module NRELssc
 
 hello() = print("Hello World adfa!")
 
-ENV["SSC_DIR"]="e:\\projects\\YangSimOpt.jl\\external\\nrelssc\\"
+ENV["SSC_DIR"]="e:\\develop\\YangSimOpt.jl\\external\\nrelssc\\"
 
 const ssc_location = ENV["SSC_DIR"]
 
 const sscdll = joinpath(ssc_location, "ssc.dll")
 
+const INVALID = 0
+const STRING = 1
+const NUMBER = 2
+const ARRAY = 3
+const MATRIX = 4
+const INPUT = 1
+const OUTPUT = 2
+const INOUT = 3
+
 function ssc_version()
     ccall((:ssc_version, sscdll), Cint, ())
+end
+
+function ssc_data_create()
+    ccall((:ssc_data_create, sscdll), Ptr{Cvoid}, ())
 end
 
 function ssc_build_info()
